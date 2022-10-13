2e2843d0d19cb919122c9999100f5bfa96433a8d
cwdatlas <ascott@carroll.edu>
2022-10-13 17:27:13 +0300

    updated Authors and fleshed out controller
    Changed controller authors to program authors. Added guidline to follow while programming Controller

--- src/main/java/com/cryptography/project/CryptController.java
+++ src/main/java/com/cryptography/project/CryptController.java
@@ -1,11 +1,30 @@
-//Author Aidan Scott
-//Co-Authors Hamza Zaher and Nikitas Valtadoros
+//Authors Aidan Scott Hamza Zaher and Nikitas Valtadoros
 //Created to mimic the AES cryopographic algorithm
 
 package com.cryptography.project;
 
 public class CryptController {
 	
-	//TODO
+	//TODO Build in all steps of controller
+	
+	//TODO step 1: deal with input from terminal and make decision based off of that
+	public static void main(String[] args) {
+		encrypt("hello");
+		decrypt("olleh");
+	}
 
+	//TODO step 2: break up text to 128 bit lengths
+		//loop encryption to encrypt entire document
+		//return encrypted document
+	private static String encrypt(String plainText) {
+		
+		return plainText;
+	}
+	//TODO step 3: break up text to 128 bit lengths
+		//Loop decryption to decrypt entire document
+		//return plain text
+	private static String decrypt(String cipherText) {
+		
+		return cipherText;
+	}
 }
--- src/main/java/com/cryptography/project/KeyManager.java
+++ src/main/java/com/cryptography/project/KeyManager.java
@@ -1,5 +1,28 @@
 package com.cryptography.project;
 
-public class KeyManager {
+public class KeyManager implements KeyManagerI{
+
+	//Constructor, so if you need to initialize any variables do it here
+	//Also, we are wanting to have our key and key variables to be in hex, so you will have
+		//to change the variable declarations to the hexNumber variable or whatever it takes
+		//to have a hex variable (add the library to dependencies in Pom file if you need to
+	public KeyManager() {
+		
+	}
+	
+	public double getRoundKey(int roundNumber) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public boolean expandKey() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public double genorateKey() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
